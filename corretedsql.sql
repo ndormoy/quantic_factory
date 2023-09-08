081a45c8ae24383d596dc78d2a79459769d51257
Noe Dormoy <noedormoy@macbook-pro-de-noe-2.home>
2023-09-08 12:35:12 +0200

    Create DB Quantic + create schema OK

---
+++ corretedsql.sql
@@ -0,0 +1,126 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+CREATE TABLE `ChannelType` (
+    -- UNSIGNED
+    `ChannelTypeID` smallint AUTO_INCREMENT NOT NULL ,
+    `Name` varchar(30)  NOT NULL ,
+    PRIMARY KEY (
+        `ChannelTypeID`
+    )
+);
+
+CREATE TABLE `EventType` (
+    -- UNSIGNED
+    `EventTypeID` smallint AUTO_INCREMENT NOT NULL ,
+    `Name` varchar(30)  NOT NULL ,
+    PRIMARY KEY (
+        `EventTypeID`
+    )
+
+CREATE TABLE `Content` (
+    -- UNSIGNED
+    `ContentID` int AUTO_INCREMENT NOT NULL ,
+    -- UNSIGNED
+    `ClientContentID` bigint  NOT NULL ,
+    `InsertDate` timestamp  NOT NULL ,
+    PRIMARY KEY (
+        `ContentID`
+    )
+);
+
+CREATE TABLE `Customer` (
+    -- AUTOINCREMENT UNSIGNED
+    `CustomerID` bigint  NOT NULL ,
+    -- UNSIGNED
+    `ClientCustomerID` bigint  NOT NULL ,
+    `InsertDate` timestamp  NOT NULL ,
+    PRIMARY KEY (
+        `CustomerID`
+    )
+);
+
+CREATE TABLE `CustomerData` (
+    -- AUTOINCREMENT UNSIGNED
+    `CustomerChannelID` bigint  NOT NULL ,
+    -- UNSIGNED
+    `CustomerID` bigint  NOT NULL ,
+    -- UNSIGNED
+    `ChannelTypeID` smallint  NOT NULL ,
+    `ChannelValue` varchar(600)  NOT NULL ,
+    `InsertDate` timestamp  NOT NULL ,
+    PRIMARY KEY (
+        `CustomerChannelID`
+    )
+);
+
+CREATE TABLE `CustomerEvent` (
+    -- AUTOINCREMENT UNSIGNED
+    `EventID` bigint  NOT NULL ,
+    -- UNSIGNED
+    `ClientEventID` bigint  NOT NULL ,
+    `InsertDate` timestamp  NOT NULL ,
+    PRIMARY KEY (
+        `EventID`
+    )
+);
+
+CREATE TABLE `CustomerEventData` (
+    -- AUTOINCREMENT UNSIGNED
+    `EventDataId` bigint  NOT NULL ,
+    -- UNSIGNED
+    `EventID` bigint  NOT NULL ,
+    -- UNSIGNED
+    `ContentID` int  NOT NULL ,
+    -- UNSIGNED
+    `CustomerID` bigint  NOT NULL ,
+    -- UNSIGNED
+    `EventTypeID` smallint  NOT NULL ,
+    `EventDate` timestamp  NOT NULL ,
+    -- UNSIGNED
+    `Quantity` smallint  NOT NULL ,
+    `InsertDate` timestamp  NOT NULL ,
+    PRIMARY KEY (
+        `EventDataId`
+    )
+);
+
+
+
+CREATE TABLE `ContentPrice` (
+    -- UNSIGNED
+    `ContentPriceID` mediumint AUTO_INCREMENT NOT NULL ,
+    -- UNSIGNED
+    `ContentID` int  NOT NULL ,
+    `Price` decimal(8,2)  NOT NULL ,
+    `Currency` char(3)  NOT NULL ,
+    `InsertDate` timestamp  NOT NULL ,
+    PRIMARY KEY (
+        `ContentPriceID`
+    )
+);
+
+
+);
+
+ALTER TABLE `CustomerData` ADD CONSTRAINT `fk_CustomerData_CustomerID` FOREIGN KEY(`CustomerID`)
+REFERENCES `Customer` (`CustomerID`);
+
+ALTER TABLE `CustomerData` ADD CONSTRAINT `fk_CustomerData_ChannelTypeID` FOREIGN KEY(`ChannelTypeID`)
+REFERENCES `ChannelType` (`ChannelTypeID`);
+
+ALTER TABLE `CustomerEventData` ADD CONSTRAINT `fk_CustomerEventData_EventID` FOREIGN KEY(`EventID`)
+REFERENCES `CustomerEvent` (`EventID`);
+
+ALTER TABLE `CustomerEventData` ADD CONSTRAINT `fk_CustomerEventData_ContentID` FOREIGN KEY(`ContentID`)
+REFERENCES `Content` (`ContentID`);
+
+ALTER TABLE `CustomerEventData` ADD CONSTRAINT `fk_CustomerEventData_CustomerID` FOREIGN KEY(`CustomerID`)
+REFERENCES `Customer` (`CustomerID`);
+
+ALTER TABLE `CustomerEventData` ADD CONSTRAINT `fk_CustomerEventData_EventTypeID` FOREIGN KEY(`EventTypeID`)
+REFERENCES `EventType` (`EventTypeID`);
+
+ALTER TABLE `ContentPrice` ADD CONSTRAINT `fk_ContentPrice_ContentID` FOREIGN KEY(`ContentID`)
+REFERENCES `Content` (`ContentID`);
+
--- test.go
+++ test.go
@@ -36,35 +36,125 @@ func getDotEnvVar(key string) string {
 }
 
 func dsn(login string, password string, ip string, dbName string) string {
-	return fmt.Sprintf("%s:%s@tcp(%s)/%s", login, password, ip, dbName)
+	// return fmt.Sprintf("%s:%s@tcp(%s)/%s", login, password, ip, dbName)
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?multiStatements=true", login, password, ip, dbName)
 }
 
+/*
+Create the database 'quantic_db' if it doesn't exist and open the connection to it
+*/
 func initializeQuanticDB(login, password, ip, dbname string) (*sql.DB, error) {
-    // Open the connection to 'mysql' database and create 'quantic_db'
-    db, err := sql.Open("mysql", dsn(login, password, ip, "mysql"))
-    if err != nil {
-        return nil, err
-    }
-    
-    ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancelfunc()
-    
-    _, err = db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
-    if err != nil {
-        db.Close() // Close the connection to 'mysql' database
-        return nil, err
-    }
-    
-    // Open the connection to 'quantic_db'
-    quanticDB, err := sql.Open("mysql", dsn(login, password, ip, dbname))
-    if err != nil {
-        db.Close() // Close the connection to 'mysql' database
-        return nil, err
-    }
-    
-    return quanticDB, nil
+	// Open the connection to 'mysql' database and create 'quantic_db'
+	db, err := sql.Open("mysql", dsn(login, password, ip, "mysql"))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+
+	_, err = db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
+	if err != nil {
+		db.Close() // Close the connection to 'mysql' database
+		return nil, err
+	}
+
+	// Open the connection to 'quantic_db'
+	quanticDB, err := sql.Open("mysql", dsn(login, password, ip, dbname))
+	if err != nil {
+		db.Close() // Close the connection to 'mysql' database
+		return nil, err
+	}
+
+	return quanticDB, nil
 }
 
+/*
+Create the database schema for the given Database
+*/
+func createDatabaseSchema(db *sql.DB) error {
+	// SQL script with table creation and constraints
+	schemaSQL := `
+
+		CREATE TABLE IF NOT EXISTS ChannelType (
+			ChannelTypeID smallint AUTO_INCREMENT NOT NULL,
+			Name varchar(30) NOT NULL,
+			PRIMARY KEY (ChannelTypeID)
+		);
+
+		CREATE TABLE IF NOT EXISTS EventType (
+			EventTypeID smallint AUTO_INCREMENT NOT NULL,
+			Name varchar(30) NOT NULL,
+			PRIMARY KEY (EventTypeID)
+		);
+
+		CREATE TABLE IF NOT EXISTS Content (
+            ContentID int AUTO_INCREMENT NOT NULL,
+            ClientContentID bigint NOT NULL,
+            InsertDate timestamp NOT NULL,
+            PRIMARY KEY (ContentID)
+        );
+
+        CREATE TABLE IF NOT EXISTS Customer (
+            CustomerID bigint NOT NULL,
+            ClientCustomerID bigint NOT NULL,
+            InsertDate timestamp NOT NULL,
+            PRIMARY KEY (CustomerID)
+        );
+
+        CREATE TABLE IF NOT EXISTS CustomerData (
+            CustomerChannelID bigint NOT NULL,
+            CustomerID bigint NOT NULL,
+            ChannelTypeID smallint NOT NULL,
+            ChannelValue varchar(600) NOT NULL,
+            InsertDate timestamp NOT NULL,
+            PRIMARY KEY (CustomerChannelID),
+            FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID),
+            FOREIGN KEY (ChannelTypeID) REFERENCES ChannelType (ChannelTypeID)
+        );
+
+        CREATE TABLE IF NOT EXISTS CustomerEvent (
+            EventID bigint NOT NULL,
+            ClientEventID bigint NOT NULL,
+            InsertDate timestamp NOT NULL,
+            PRIMARY KEY (EventID)
+        );
+
+        CREATE TABLE IF NOT EXISTS CustomerEventData (
+            EventDataId bigint NOT NULL,
+            EventID bigint NOT NULL,
+            ContentID int NOT NULL,
+            CustomerID bigint NOT NULL,
+            EventTypeID smallint NOT NULL,
+            EventDate timestamp NOT NULL,
+            Quantity smallint NOT NULL,
+            InsertDate timestamp NOT NULL,
+            PRIMARY KEY (EventDataId),
+            FOREIGN KEY (EventID) REFERENCES CustomerEvent (EventID),
+            FOREIGN KEY (ContentID) REFERENCES Content (ContentID),
+            FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID),
+            FOREIGN KEY (EventTypeID) REFERENCES EventType (EventTypeID)
+        );
+
+        CREATE TABLE IF NOT EXISTS ContentPrice (
+            ContentPriceID mediumint AUTO_INCREMENT NOT NULL,
+            ContentID int NOT NULL,
+            Price decimal(8,2) NOT NULL,
+            Currency char(3) NOT NULL,
+            InsertDate timestamp NOT NULL,
+            PRIMARY KEY (ContentPriceID),
+            FOREIGN KEY (ContentID) REFERENCES Content (ContentID)
+        );
+
+        
+    `
+
+	_, err := db.Exec(schemaSQL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	fmt.Println("Hello, World!aaaa")
@@ -72,15 +162,18 @@ func main() {
 	var password string = getDotEnvVar("PASSWORD")
 	var ip string = getDotEnvVar("IP")
 	var dbname string = "quantic_db"
-	fmt.Println("LOGIN: " + login)
-	fmt.Println("PASSWORD: " + password)
-	fmt.Println("IP: " + ip)
 
 	quanticDB, err := initializeQuanticDB(login, password, ip, dbname)
-    if err != nil {
-        log.Printf("Error %s when initializing 'quantic_db' DB\n", err)
-        return
-    }
-    defer quanticDB.Close() // Close the connection to 'quantic_db'
+	if err != nil {
+		log.Printf("Error %s when initializing 'quantic_db' DB\n", err)
+		return
+	}
+	defer quanticDB.Close() // Close the connection to 'quantic_db'
+
+	// Create the database schema
+	if err := createDatabaseSchema(quanticDB); err != nil {
+		log.Printf("Error %s when creating database schema\n", err)
+		return
+	}
 
 }
