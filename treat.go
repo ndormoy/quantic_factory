01ecaa90be1faae3d5d1d3d24b60ae7a4b9c700d
Noe Dormoy <noedormoy@macbook-pro-de-noe-2.home>
2023-09-13 17:17:30 +0200

    Conversion OK corrected

--- treat.go
+++ treat.go
@@ -16,50 +16,6 @@ import (
 	"database/sql"
 )
 
-// func mapWithCustIDPurchaseJOIN(db *sql.DB) {
-// 	// Query to retrieve CustomerEventData records with EventTypeID = 6 and ContentID
-// 	query := `
-// 		SELECT c.CustomerID, ce.ContentID
-// 		FROM CustomerEventData ce
-// 		INNER JOIN Customer c ON ce.CustomerID = c.CustomerID
-// 		WHERE ce.EventTypeID = 6
-// 	`
-
-// 	rows, err := db.Query(query)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer rows.Close()
-
-// 	// Create a map to store CustomerID and total Price
-// 	customerPriceMap := make(map[int64]float64)
-
-// 	// Iterate through the rows to get ContentID and Price
-// 	for rows.Next() {
-// 		var customerID, contentID int64
-// 		err := rows.Scan(&customerID, &contentID)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-
-// 		// Query to retrieve Price from ContentPrice
-// 		priceQuery := "SELECT Price FROM ContentPrice WHERE ContentID = ?"
-// 		var price float64
-// 		err = db.QueryRow(priceQuery, contentID).Scan(&price)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-
-// 		// Add the Price to the customer's total Price
-// 		customerPriceMap[customerID] += price
-// 	}
-
-// 	// Print the map with CustomerID and total Price
-// 	for customerID, totalPrice := range customerPriceMap {
-// 		fmt.Printf("CustomerID: %d, Total Price: %.2f\n", customerID, totalPrice)
-// 	}
-// }
-
 /*
 This function get back the ContentID in CustomerEventData where EventTypeID == 6 (purchase)
 */
@@ -96,112 +52,6 @@ This function create and return a map with CustomerID and their purchases we mul
 We use ContentID from the function getContentIDFromPurchase to retrieve the Price from ContentPrice.
 We return something like this, a map : CustomerIDs = moneySpent
 */
-
-// func calculateTotalPurchaseAmounts(db *sql.DB, contentIDs []int64) (map[int64]float64, error) {
-// 	customerPurchaseAmounts := make(map[int64]float64)
-// 	processedPurchases := make(map[int64]map[int64]bool) // Track processed purchases per customer
-
-// 	for _, contentID := range contentIDs {
-// 		query := `
-//             SELECT CustomerID, Quantity
-//             FROM CustomerEventData
-//             WHERE ContentID = ? AND EventTypeID = 6
-//         `
-// 		rows, err := db.Query(query, contentID)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		defer rows.Close()
-
-// 		for rows.Next() {
-// 			var customerID, quantity int64
-// 			if err := rows.Scan(&customerID, &quantity); err != nil {
-// 				return nil, err
-// 			}
-
-// 			// Check if this purchase has already been processed for this customer
-// 			if processedPurchases[customerID] == nil {
-// 				processedPurchases[customerID] = make(map[int64]bool)
-// 			}
-// 			if processedPurchases[customerID][contentID] {
-// 				continue // Skip if already processed
-// 			}
-
-// 			// Calculate the purchase amount for this specific purchase
-// 			priceQuery := "SELECT Price FROM ContentPrice WHERE ContentID = ?"
-// 			var price float64
-// 			err := db.QueryRow(priceQuery, contentID).Scan(&price)
-// 			if err != nil {
-// 				return nil, err
-// 			}
-// 			purchaseAmount := price * float64(quantity)
-
-// 			// Add the purchase amount to the customer's total
-// 			customerPurchaseAmounts[customerID] += purchaseAmount
-// 			processedPurchases[customerID][contentID] = true
-// 			// fmt.Printf("Added purchase amount %.2f to CustomerID %d\n", purchaseAmount, customerID)
-// 		}
-// 	}
-// 	return customerPurchaseAmounts, nil
-// }
-
-// func calculateTotalPurchaseAmounts(db *sql.DB, contentIDs []int64, currencyMap map[int64]string) (map[int64]float64, error) {
-// 	customerPurchaseAmounts := make(map[int64]float64)
-// 	processedPurchases := make(map[int64]map[int64]bool) // Track processed purchases per customer
-
-// 	for _, contentID := range contentIDs {
-// 		query := `
-//             SELECT CustomerID, Quantity
-//             FROM CustomerEventData
-//             WHERE ContentID = ? AND EventTypeID = 6
-//         `
-// 		rows, err := db.Query(query, contentID)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		defer rows.Close()
-
-// 		for rows.Next() {
-// 			var customerID, quantity int64
-// 			if err := rows.Scan(&customerID, &quantity); err != nil {
-// 				return nil, err
-// 			}
-
-// 			// Check if this purchase has already been processed for this customer
-// 			if processedPurchases[customerID] == nil {
-// 				processedPurchases[customerID] = make(map[int64]bool)
-// 			}
-// 			if processedPurchases[customerID][contentID] {
-// 				continue // Skip if already processed
-// 			}
-
-// 			// Calculate the purchase amount for this specific purchase
-// 			priceQuery := "SELECT Price FROM ContentPrice WHERE ContentID = ?"
-// 			var price float64
-// 			err := db.QueryRow(priceQuery, contentID).Scan(&price)
-// 			if err != nil {
-// 				return nil, err
-// 			}
-// 			purchaseAmount := price * float64(quantity)
-
-// 			// Convert the purchase amount to euros
-// 			var currency string = currencyMap[contentID]
-// 			if currency != "EUR" {
-// 				convertedAmount, err := convertToEUR(purchaseAmount, currency)
-// 				if err != nil {
-// 					return nil, err
-// 				}
-// 				purchaseAmount = convertedAmount
-// 			}
-
-// 			// Add the purchase amount to the customer's total
-// 			customerPurchaseAmounts[customerID] += purchaseAmount
-// 			processedPurchases[customerID][contentID] = true
-// 			// fmt.Printf("Added purchase amount %.2f to CustomerID %d\n", purchaseAmount, customerID)
-// 		}
-// 	}
-// 	return customerPurchaseAmounts, nil
-// }
 
 func calculateTotalPurchaseAmounts(db *sql.DB, contentIDs []int64, currencyMap map[int64]string) (map[int64]float64, error) {
 	customerPurchaseAmounts := make(map[int64]float64)
@@ -264,8 +114,6 @@ func calculateTotalPurchaseAmounts(db *sql.DB, contentIDs []int64, currencyMap m
 	}
 	return customerPurchaseAmounts, nil
 }
-
-
 
 func getCurrencyForCustomers(db *sql.DB, contentIDs []int64) (map[int64]string, error) {
 	currencyMap := make(map[int64]string)
--- utils.go
+++ utils.go
@@ -70,27 +70,27 @@ func getDotEnvVar(key string) string {
 func convertToEUR(price float64, currency string) (float64, error) {
 	// Define exchange rates for some currencies (you can extend this list)
 	exchangeRates := map[string]float64{
-		"EUR": 1.07,     // Euro
-		"USD": 1.18,     // US Dollar
-		"BRL": 5.30,     // Brazilian Real
-		"MYR": 5.03,     // Malaysian Ringgit
-		"IDR": 16500.95, // Indonesian Rupiah
-		"CNY": 7.72,     // Chinese Yuan
-		"PLN": 4.62,     // Polish Zloty
-		"RSD": 117.28,   // Serbian Dinar
-		"PKR": 316.89,   // Pakistani Rupee
-		"GTQ": 8.45,     // Guatemalan Quetzal
-		"PHP": 61.02,    // Philippines Pes
-		"BAM": 1.95,     // Bosnian Marks
-		"ALL": 106.37,   // Albanian Lek
-		"RUB": 103.55,   // Russian Ruble
-		"HNL": 26.45,    // Honduran Lempira
-		"JPY": 158.33,   // Japanese Yen
-		"TND": 3.36,     // Tunisian Dinar
-		"NOK": 11.49,    // Norwegian Krone
-		"KRW": 1425.36,  // Won south korean
-		"COP": 4258.73,  //Colombian Peso
-		"MXN": 18.39,    // Mexico peso
+		"EUR": 1.00,         // Euro
+		"USD": 0.93,         // US Dollar
+		"BRL": 0.19,         // Brazilian Real
+		"MYR": 0.20,         // Malaysian Ringgit
+		"IDR": 0.000061,     // Indonesian Rupiah
+		"CNY": 0.13,         // Chinese Yuan
+		"PLN": 0.22,         // Polish Zloty
+		"RSD": 0.0085,       // Serbian Dinar
+		"PKR": 0.0032,       // Pakistani Rupee
+		"GTQ": 0.12,         // Guatemalan Quetzal
+		"PHP": 0.016,        // Philippines Pes
+		"BAM": 0.51,         // Bosnian Marks
+		"ALL": 0.0094108537, // Albanian Lek
+		"RUB": 0.0097,       // Russian Ruble
+		"HNL": 0.038,        // Honduran Lempira
+		"JPY": 0.0063,       // Japanese Yen
+		"TND": 0.30,         // Tunisian Dinar
+		"NOK": 0.087,        // Norwegian Krone
+		"KRW": 0.00070,      // Won south korean
+		"COP": 0.00023,      //Colombian Peso
+		"MXN": 0.054,        // Mexico peso
 		// Add more currencies and their exchange rates here
 	}
 
@@ -101,6 +101,6 @@ func convertToEUR(price float64, currency string) (float64, error) {
 	}
 
 	// Convert the price to euros
-	priceInEUR := price / rate
+	priceInEUR := price * rate
 	return priceInEUR, nil
 }
