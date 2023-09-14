e3c7a7c03b40e22aada2d7d3859fd02714807965
Noe Dormoy <noedormoy@macbook-pro-de-noe.home>
2023-09-14 11:30:10 +0200

    Changing of quantile function, working now, if problem take this version, the last function is commented and still here

--- quantile.go
+++ quantile.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	// "math"
 	"sort"
 )
 
@@ -50,60 +51,116 @@ func createBestClientMap(spentValues []float64, moneySpentSlice []CustomerSpent)
 	Function who create all the quantiles for all the customers, 2.5% by 2.5 until the end
 */
 
-func CreateAllQuantileMap(spentValues []float64) map[float64]QuantileInfo {
-	// Sort the spentValues array
-	sort.Float64s(spentValues)
-	quantiles := GenerateQuantiles(0, 1, 0.025)
-	// Create a map to store quantile information
-	quantileInfoMap := make(map[float64]QuantileInfo)
+// func CreateAllQuantileMap(spentValues []float64) map[float64]QuantileInfo {
+// 	// Sort the spentValues array
+// 	sort.Float64s(spentValues)
+// 	quantiles := GenerateQuantiles(0, 1, 0.025)
+// 	// Create a map to store quantile information
+// 	quantileInfoMap := make(map[float64]QuantileInfo)
 
-	for i, q := range quantiles {
-		quantile := quantiles[i]
-		nextQuantile := 1.0
-		if i < len(quantiles)-1 {
-			nextQuantile = quantiles[i+1]
-		}
-		// Calculate the index range for the current quantile, rounding appropriately
-		startIndex := int(float64(len(spentValues)-1)*quantile + 0.5)
-		endIndex := int(float64(len(spentValues)-1)*nextQuantile + 0.5)
-		// Initialize minRevenue and maxRevenue with the first value in the current quantile
-		minRevenue := spentValues[startIndex]
-		maxRevenue := spentValues[startIndex]
-		numClients := endIndex - startIndex
-		// Calculate max and min revenue in the current quantile
-		for j := startIndex; j < endIndex; j++ {
-			if spentValues[j] > maxRevenue {
-				maxRevenue = spentValues[j]
-			}
-			if spentValues[j] < minRevenue {
-				minRevenue = spentValues[j]
-			}
-		}
-		// Store quantile information in the map
-		quantileInfoMap[q] = QuantileInfo{
-			NumClients: numClients,
-			MaxRevenue: maxRevenue,
-			MinRevenue: minRevenue,
-		}
-		// fmt.Printf("%.2f%% Quantile: %.2f\n", q*100, quantileValues[i])
-	}
+// 	for i, q := range quantiles {
+// 		quantile := quantiles[i]
+// 		nextQuantile := 1.0
+// 		if i < len(quantiles)-1 {
+// 			nextQuantile = quantiles[i+1]
+// 		}
+// 		// Calculate the index range for the current quantile, rounding appropriately
+// 		// startIndex := int(float64(len(spentValues)-1)*quantile + 0.5)
+// 		// endIndex := int(float64(len(spentValues)-1)*nextQuantile + 0.5)
+// 		startIndex := int(math.Round(float64(len(spentValues)-1) * quantile))
+// 		endIndex := int(math.Round(float64(len(spentValues)-1) * nextQuantile))
+// 		// endIndex := int(math.Ceil(float64(len(spentValues)) * nextQuantile))
 
-	// Sort the quantile keys
-	sortedQuantiles := make([]float64, 0, len(quantileInfoMap))
-	for q := range quantileInfoMap {
-		sortedQuantiles = append(sortedQuantiles, q)
-	}
-	sort.Float64s(sortedQuantiles)
-	// Access quantile information from the map
-	for _, q := range sortedQuantiles {
-		info := quantileInfoMap[q]
-		fmt.Printf("%.2f%% Quantile Info:\n", q*100)
-		fmt.Printf("Number of clients: %d\n", info.NumClients)
-		fmt.Printf("Max Revenue: %.2f\n", info.MaxRevenue)
-		fmt.Printf("Min Revenue: %.2f\n\n", info.MinRevenue)
-	}
-	return quantileInfoMap
+
+// 		// Initialize minRevenue and maxRevenue with the first value in the current quantile
+// 		minRevenue := spentValues[startIndex]
+// 		maxRevenue := spentValues[startIndex]
+// 		numClients := endIndex - startIndex
+// 		// Calculate max and min revenue in the current quantile
+// 		for j := startIndex; j < endIndex; j++ {
+// 			if spentValues[j] > maxRevenue {
+// 				maxRevenue = spentValues[j]
+// 			}
+// 			if spentValues[j] < minRevenue {
+// 				minRevenue = spentValues[j]
+// 			}
+// 		}
+// 		// Store quantile information in the map
+// 		quantileInfoMap[q] = QuantileInfo{
+// 			NumClients: numClients,
+// 			MaxRevenue: maxRevenue,
+// 			MinRevenue: minRevenue,
+// 		}
+// 	}
+
+// 	// Sort the quantile keys
+// 	sortedQuantiles := make([]float64, 0, len(quantileInfoMap))
+// 	for q := range quantileInfoMap {
+// 		sortedQuantiles = append(sortedQuantiles, q)
+// 	}
+// 	sort.Float64s(sortedQuantiles)
+// 	// Access quantile information from the map
+// 	for _, q := range sortedQuantiles {
+// 		info := quantileInfoMap[q]
+// 		fmt.Printf("%.2f%% Quantile Info:\n", q*100)
+// 		fmt.Printf("Number of clients: %d\n", info.NumClients)
+// 		fmt.Printf("Max Revenue: %.2f\n", info.MaxRevenue)
+// 		fmt.Printf("Min Revenue: %.2f\n\n", info.MinRevenue)
+// 	}
+// 	return quantileInfoMap
+// }
+
+func CalculateQuantilesNearestRank(spentValues []float64, numQuantiles int) (map[float64]QuantileInfo, []float64) {
+    // Sort the spentValues array
+    sort.Float64s(spentValues)
+
+    // Create a map to store quantile information
+    quantileInfoMap := make(map[float64]QuantileInfo)
+
+    // Calculate the number of clients per quantile
+    clientsPerQuantile := len(spentValues) / numQuantiles
+
+    // Create a slice to store quantiles for sorting
+    quantiles := make([]float64, numQuantiles)
+
+    for i := 0; i < numQuantiles; i++ {
+        // Calculate the index range for the current quantile
+        startIndex := i * clientsPerQuantile
+        endIndex := (i + 1) * clientsPerQuantile
+        if i == numQuantiles-1 {
+            endIndex = len(spentValues)
+        }
+
+        // Initialize minRevenue and maxRevenue with the first value in the current quantile
+        minRevenue := spentValues[startIndex]
+        maxRevenue := spentValues[startIndex]
+        numClients := endIndex - startIndex
+
+        // Calculate max and min revenue in the current quantile
+        for j := startIndex; j < endIndex; j++ {
+            if spentValues[j] > maxRevenue {
+                maxRevenue = spentValues[j]
+            }
+            if spentValues[j] < minRevenue {
+                minRevenue = spentValues[j]
+            }
+        }
+
+        // Calculate the quantile value
+        quantile := float64(i+1) / float64(numQuantiles)
+        quantiles[i] = quantile
+
+        // Store quantile information in the map
+        quantileInfoMap[quantile] = QuantileInfo{
+            NumClients: numClients,
+            MaxRevenue: maxRevenue,
+            MinRevenue: minRevenue,
+        }
+    }
+
+    return quantileInfoMap, quantiles
 }
+
 
 /*
 	Function to generate all of the quantile number automatically, with a start a end and step
